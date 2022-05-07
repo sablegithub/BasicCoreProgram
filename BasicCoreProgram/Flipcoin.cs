6a069baf4190becbdffc37a321bb892f64cc0c19
vaibhav sable <vaibhavsable52@gmail.com>
2022-05-06 22:42:15 -0700

    Added Flip Coin

---
+++ BasicCoreProgram/Flipcoin.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicCoreProgram
+{
+    internal class Flipcoin
+    {
+        public void FlipCoin()
+        {
+            Random rnd = new Random();
+            int result = rnd.Next(0, 2);
+
+            if (result < 0.5)
+            {
+                Console.WriteLine("Tails!");
+
+            }
+            else if (result == 1)
+            {
+                Console.WriteLine("Heads!");
+
+            }
+        }
+    }
+}
--- BasicCoreProgram/Program.cs
+++ BasicCoreProgram/Program.cs
@@ -1,2 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using BasicCoreProgram;
 Console.WriteLine("Welcome to basic core program");
+Flipcoin objFlipCoin=new Flipcoin();
+objFlipCoin.FlipCoin();
+
